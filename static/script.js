fee2168f95cdf0624e9ac75803ca08128113aa18
khuynh <khuynh@localhost>
2021-08-20 19:13:47 -0700

    Checkpoint 2

--- static/script.js
+++ static/script.js
@@ -3,9 +3,9 @@ class Util {
         throw new Error("attempted to construct Util");
     }
     static newDiv(nam) {
-        const rv = document.createElement("div");
-        rv.className = nam;
-        return rv;
+        const ret = document.createElement("div");
+        ret.className = nam;
+        return ret;
     }
 }
 class FrameSongDisplay {
@@ -37,13 +37,20 @@ class FrameSongDisplay {
     }
 }
 class FrameWaveDisplay {
-    constructor(app) {
-        this.cv = null;
+    constructor(App) {
+        this.App = App;
         this.waveDiv = Util.newDiv("fwd-wave");
-        app.appendChild(this.waveDiv);
+        this.App.app.appendChild(this.waveDiv);
+        this.cv = document.createElement("canvas");
+        this.cv.className = "waveformscanvas";
+        this.waveDiv.appendChild(this.cv);
+        const cvBCR = this.cv.getBoundingClientRect();
+        this.cv.width = cvBCR.width;
+        this.cv.height = cvBCR.height;
+        this.addEventListenersToCanvas();
         return this;
     }
-    // Return true if little-endian.
+    // Helper fn that returns true if host is little-endian.
     isLE() {
         const ab = new ArrayBuffer(2);
         const uint8t_arr = new Uint8Array(ab);
@@ -54,8 +61,9 @@ class FrameWaveDisplay {
             return false;
         return true;
     }
-    // Generate a series of bar heights.
+    // Helper fn to generate a series of bar heights.
     generateSlots(dv, width) {
+        const isLE = this.isLE();
         const slots = new Array();
         const nBytesPerSlot = Math.floor(dv.byteLength / width);
         const nFloatsPerSlot = Math.floor(nBytesPerSlot / 4);
@@ -64,30 +72,21 @@ class FrameWaveDisplay {
             let avg = 0;
             for (let j = 0; j < nFloatsPerSlot; j++) {
                 const offset = Math.floor((j * 4 + i * nBytesPerSlot) / 4) * 4;
-                const f = dv.getFloat32(offset, this.isLE());
-                if (!Number.isNaN(f))
-                    avg += f;
-                else
-                    console.log("WARN: generateSlots encounted a NaN");
+                const f = dv.getFloat32(offset, isLE);
+                avg += f;
             }
             avg /= nFloatsPerSlot;
             slots.push(avg);
         }
         return slots;
     }
-    // Draw slots onto the canvas.
+    // Helper fn to draw slots onto the canvas.
     drawSlots(slots) {
         this.normaliseVolume(slots);
-        if (!this.cv) {
-            this.cv = document.createElement("canvas");
-            this.cv.className = "waveformscanvas";
-            this.waveDiv.appendChild(this.cv);
-            const cvBCR = this.cv.getBoundingClientRect();
-            this.cv.width = cvBCR.width;
-            this.cv.height = cvBCR.height;
-        }
         const ctx = this.cv.getContext("2d");
+        ctx.clearRect(0, 0, this.cv.width, this.cv.height);
         ctx.lineWidth = this.cv.width / slots.length;
+        ctx.strokeStyle = "#ff0000ff";
         const ctxHeight = this.cv.height;
         for (let i = 0; i < slots.length; i++) {
             const height = -slots[i] * ctxHeight / 2 + ctxHeight / 2;
@@ -97,7 +96,7 @@ class FrameWaveDisplay {
             ctx.stroke();
         }
     }
-    // Increase the amplitude of the slots' samples.
+    // Helper fn to increase the amplitude of the slots' samples.
     normaliseVolume(slots) {
         let max = 0;
         for (let i = 0; i < slots.length; i++) {
@@ -109,7 +108,6 @@ class FrameWaveDisplay {
             slots[i] *= scale;
     }
     setTrack(path) {
-        return; // hack: perf issues
         fetch(path).then(resp => {
             if (resp.ok)
                 return resp.arrayBuffer();
@@ -118,7 +116,6 @@ class FrameWaveDisplay {
         }).then(buf => {
             return new AudioContext().decodeAudioData(buf);
         }).then(ab => {
-            //Util.startPlayBack(ab);
             const f32 = ab.getChannelData(0);
             const dv = new DataView(f32.buffer);
             return this.generateSlots(dv, window.innerWidth);
@@ -128,23 +125,37 @@ class FrameWaveDisplay {
             console.log("audio malfunction: ", e);
         });
     }
+    // Helper for constructor to add an event listener to canvas.
+    addEventListenersToCanvas() {
+        this.cv.addEventListener("click", (e) => {
+            const cvBCR = this.cv.getBoundingClientRect();
+            const cx = e.clientX - cvBCR.left;
+            const cy = e.clientY - cvBCR.top;
+            const frac = cx / cvBCR.width * this.App.audio.duration;
+            this.App.audio.currentTime = frac;
+            this.App.audio.play();
+            const plays = this.App.app.getElementsByClassName("play");
+            if (plays.length === 1) {
+                const play = plays[0];
+                play.innerText = "Pause";
+            }
+        });
+    }
 }
 class FrameCommentsDisplay {
     constructor(App) {
-        const commsDiv = Util.newDiv("fcd-comms");
+        this.commsDiv = Util.newDiv("fcd-comms");
         const commbarhost = App.getCommbarhost();
-        commbarhost.appendChild(commsDiv);
+        commbarhost.appendChild(this.commsDiv);
+        this.populateSelf();
+    }
+    populateSelf() {
+        // todo
     }
 }
 class Bar {
-    constructor(App, fsd, fwd) {
-        const bar = Util.newDiv("bar");
-        const play = Util.newDiv("play");
-        const ff = Util.newDiv("ff");
-        const rw = Util.newDiv("rw");
-        bar.appendChild(ff);
-        bar.appendChild(play);
-        bar.appendChild(rw);
+    // Helper for constructor to add event listeners to bar buttons.
+    addEventListeners(App, play, ff, rw, fsd, fwd) {
         play.innerText = "Play";
         play.addEventListener("click", () => {
             if (Util.currTrack && Util.currTrack.filepath) {
@@ -153,10 +164,14 @@ class Bar {
                     App.audio.play();
                 }
                 else { // toggle
-                    if (App.audio.paused)
+                    if (App.audio.paused) {
                         App.audio.play();
-                    else
+                        play.innerText = "Pause";
+                    }
+                    else {
                         App.audio.pause();
+                        play.innerText = "Play";
+                    }
                 }
             }
         });
@@ -168,13 +183,32 @@ class Bar {
                     if (App.tracksIndex > Object.keys(tracks).length)
                         App.tracksIndex = 1; // {1: {}, 2: {}}
                     App.populateAudio(App.tracksIndex + "", fsd, fwd);
-                    //if (App.audio)  // bug ?
-                    //	App.audio.pause();
-                    //App.audio = new Audio(Util.currTrack.filepath);
-                    //App.audio.play();
+                    play.innerText = "Pause";
                 }
             });
         });
+        rw.innerText = "Rewind";
+        rw.addEventListener("click", () => {
+            App.tracks.then((tracks) => {
+                if (App.tracksIndex !== -1) {
+                    App.tracksIndex--;
+                    if (App.tracksIndex <= 0)
+                        App.tracksIndex = Object.keys(tracks).length;
+                    App.populateAudio(App.tracksIndex + "", fsd, fwd);
+                    play.innerText = "Pause";
+                }
+            });
+        });
+    }
+    constructor(App, fsd, fwd) {
+        const bar = Util.newDiv("bar");
+        const play = Util.newDiv("play");
+        const ff = Util.newDiv("ff");
+        const rw = Util.newDiv("rw");
+        bar.appendChild(ff);
+        bar.appendChild(play);
+        bar.appendChild(rw);
+        this.addEventListeners(App, play, ff, rw, fsd, fwd);
         const commbarhost = App.getCommbarhost();
         commbarhost.appendChild(bar);
     }
@@ -187,12 +221,11 @@ class App {
         this.app = Util.newDiv("app");
         document.body.appendChild(this.app);
         const fsd = new FrameSongDisplay(this.app);
-        const fwd = new FrameWaveDisplay(this.app);
+        const fwd = new FrameWaveDisplay(this);
         new FrameCommentsDisplay(this);
         new Bar(this, fsd, fwd);
         this.getAudio();
         this.populateAudio("1", fsd, fwd);
-        //this.tracks.then(stuff => {console.log(stuff);});
     }
     getCommbarhost() {
         if (!this.commbarhost) {
@@ -207,6 +240,42 @@ class App {
                 return resp.json();
             }
         });
+    }
+    // Helper for setupWatcher to find the shadow element.
+    getShadow(fwd) {
+        const shadows = this.app.getElementsByClassName("shadow");
+        let shadow = null;
+        if (shadows.length < 1) {
+            shadow = Util.newDiv("shadow");
+            shadow.style.position = "absolute";
+            shadow.style.backgroundColor = "rgba(255, 160, 0, 0.5)";
+            fwd.waveDiv.appendChild(shadow);
+        }
+        else
+            shadow = shadows[0];
+        return shadow;
+    }
+    setupWatcher(fwd) {
+        const shadow = this.getShadow(fwd);
+        clearInterval(this.watcherID);
+        this.watcherID = setInterval(() => {
+            const currTime = this.audio.currentTime;
+            const duration = this.audio.duration;
+            const frac = currTime / duration;
+            const cvs = this.app.getElementsByClassName("waveformscanvas");
+            if (cvs.length === 1) {
+                const cv = cvs[0];
+                const cvBCR = cv.getBoundingClientRect();
+                const width = frac * cvBCR.width;
+                const nx = cvBCR.left + window.scrollX;
+                const ny = cvBCR.top + window.scrollY;
+                const height = cvBCR.height;
+                shadow.style.left = nx + "px";
+                shadow.style.top = ny + "px";
+                shadow.style.height = height + "px";
+                shadow.style.width = width + "px";
+            }
+        }, 100);
     }
     populateAudio(id, fsd, fwd) {
         if (this.audio)
@@ -224,7 +293,7 @@ class App {
             else
                 this.audio.src = track.filepath;
             this.audio.play();
-            //}
+            this.setupWatcher(fwd);
         });
     }
 }
--- static/script.ts
+++ static/script.ts
@@ -49,10 +49,11 @@ class FrameSongDisplay {
 }
 
 class FrameWaveDisplay {
+	App: App;
 	waveDiv: HTMLDivElement;
 	cv: HTMLCanvasElement;
 	
-	// Return true if little-endian.
+	// Helper fn that returns true if host is little-endian.
 	isLE(): boolean {
 		const ab = new ArrayBuffer(2);
 		const uint8t_arr = new Uint8Array(ab);
@@ -63,8 +64,9 @@ class FrameWaveDisplay {
 			return false;
 		return true;
 	}
-	// Generate a series of bar heights.
+	// Helper fn to generate a series of bar heights.
 	generateSlots(dv: DataView, width: number) {
+		const isLE = this.isLE();
 		const slots = new Array();
 		const nBytesPerSlot = Math.floor(dv.byteLength / width);
 		const nFloatsPerSlot = Math.floor(nBytesPerSlot / 4);
@@ -73,31 +75,22 @@ class FrameWaveDisplay {
 			let avg = 0;
 			for (let j = 0; j < nFloatsPerSlot; j++) {
 				const offset = Math.floor((j * 4 + i * nBytesPerSlot) / 4) * 4;
-				const f = dv.getFloat32(offset, this.isLE());
-				if (!Number.isNaN(f))
-					avg += f;
-				else
-					console.log("WARN: generateSlots encounted a NaN");
+				const f = dv.getFloat32(offset, isLE);
+				avg += f;
 			}
 			avg /= nFloatsPerSlot;
 			slots.push(avg);
 		}
 		return slots;
 	}
-	// Draw slots onto the canvas.
+	// Helper fn to draw slots onto the canvas.
 	drawSlots(slots: Array<number>): void {
 		this.normaliseVolume(slots);
-		if (!this.cv) {
-			this.cv = document.createElement("canvas");
-			this.cv.className = "waveformscanvas";
-			this.waveDiv.appendChild(this.cv);
-			const cvBCR = this.cv.getBoundingClientRect();
-			this.cv.width = cvBCR.width;
-			this.cv.height = cvBCR.height;
-		}
 		
 		const ctx = this.cv.getContext("2d");
+		ctx.clearRect(0, 0, this.cv.width, this.cv.height);
 		ctx.lineWidth = this.cv.width / slots.length;
+		ctx.strokeStyle = "#ff0000ff";
 		const ctxHeight = this.cv.height;
 		for (let i = 0; i < slots.length; i++) {
 			const height = -slots[i] * ctxHeight / 2 + ctxHeight / 2;
@@ -107,7 +100,7 @@ class FrameWaveDisplay {
 			ctx.stroke();
 		}
 	}
-	// Increase the amplitude of the slots' samples.
+	// Helper fn to increase the amplitude of the slots' samples.
 	normaliseVolume(slots: Array<number>): void {
 		let max = 0;
 		for (let i = 0; i < slots.length; i++) {
@@ -119,7 +112,6 @@ class FrameWaveDisplay {
 			slots[i] *= scale;
 	}
 	setTrack(path: string): void {
-		return;  // hack: perf issues
 		fetch(path).then(resp => {
 			if (resp.ok)
 				return resp.arrayBuffer();
@@ -128,8 +120,6 @@ class FrameWaveDisplay {
 		}).then(buf => {
 			return new AudioContext().decodeAudioData(buf);
 		}).then(ab => {  // AudioBuffer
-			//Util.startPlayBack(ab);
-			
 			const f32 = ab.getChannelData(0);
 			const dv = new DataView(f32.buffer);
 			return this.generateSlots(dv, window.innerWidth);
@@ -139,32 +129,59 @@ class FrameWaveDisplay {
 			console.log("audio malfunction: ", e);
 		});
 	}
-	
-	constructor(app: HTMLDivElement) {
-		this.cv = null;
+	// Helper for constructor to add an event listener to canvas.
+	addEventListenersToCanvas(): void {
+		this.cv.addEventListener("click", (e) => {
+			const cvBCR = this.cv.getBoundingClientRect();
+			const cx = e.clientX - cvBCR.left;
+			const cy = e.clientY - cvBCR.top;
+			const frac = cx / cvBCR.width * this.App.audio.duration;
+			this.App.audio.currentTime = frac;
+			this.App.audio.play();
+			
+			const plays = this.App.app.getElementsByClassName("play");
+			if (plays.length === 1) {
+				const play = <HTMLDivElement> plays[0];
+				play.innerText = "Pause";
+			}
+		});
+	}
+	constructor(App: App) {
+		this.App = App;
 		this.waveDiv = Util.newDiv("fwd-wave");
-		app.appendChild(this.waveDiv);
+		this.App.app.appendChild(this.waveDiv);
+		
+		this.cv = document.createElement("canvas");
+		this.cv.className = "waveformscanvas";
+		this.waveDiv.appendChild(this.cv);
+		const cvBCR = this.cv.getBoundingClientRect();
+		this.cv.width = cvBCR.width;
+		this.cv.height = cvBCR.height;
+		
+		this.addEventListenersToCanvas();
+		
 		return this;
 	}
 }
 
 class FrameCommentsDisplay {
+	commsDiv: HTMLDivElement;
+	populateSelf(): void {
+		// todo
+	}
 	constructor(App: App) {
-		const commsDiv = Util.newDiv("fcd-comms");
+		this.commsDiv = Util.newDiv("fcd-comms");
 		const commbarhost = App.getCommbarhost();
-		commbarhost.appendChild(commsDiv);
+		commbarhost.appendChild(this.commsDiv);
+		
+		this.populateSelf();
 	}
 }
 
 class Bar {
-	constructor(App: App, fsd: FrameSongDisplay, fwd: FrameWaveDisplay) {
-		const bar = Util.newDiv("bar");
-		const play = Util.newDiv("play");
-		const ff = Util.newDiv("ff");
-		const rw = Util.newDiv("rw");
-		bar.appendChild(ff);
-		bar.appendChild(play);
-		bar.appendChild(rw);
+	// Helper for constructor to add event listeners to bar buttons.
+	addEventListeners(App: App, play: HTMLDivElement, ff: HTMLDivElement,
+		rw: HTMLDivElement, fsd: FrameSongDisplay, fwd: FrameWaveDisplay) {
 		play.innerText = "Play"
 		play.addEventListener("click", () => {
 			if (Util.currTrack && Util.currTrack.filepath) {
@@ -172,10 +189,13 @@ class Bar {
 					App.audio = new Audio(Util.currTrack.filepath);
 					App.audio.play();
 				} else {  // toggle
-					if (App.audio.paused)
+					if (App.audio.paused) {
 						App.audio.play();
-					else
+						play.innerText = "Pause";
+					} else {
 						App.audio.pause();
+						play.innerText = "Play";
+					}
 				}
 			}
 		});
@@ -187,13 +207,32 @@ class Bar {
 					if (App.tracksIndex > Object.keys(tracks).length)
 						App.tracksIndex = 1;  // {1: {}, 2: {}}
 					App.populateAudio(App.tracksIndex + "", fsd, fwd);
-					//if (App.audio)  // bug ?
-					//	App.audio.pause();
-					//App.audio = new Audio(Util.currTrack.filepath);
-					//App.audio.play();
+					play.innerText = "Pause";
 				}
 			});
 		});
+		rw.innerText = "Rewind";
+		rw.addEventListener("click", () => {
+			App.tracks.then((tracks) => {
+				if (App.tracksIndex !== -1) {
+					App.tracksIndex--;
+					if (App.tracksIndex <= 0)
+						App.tracksIndex = Object.keys(tracks).length;
+					App.populateAudio(App.tracksIndex + "", fsd, fwd);
+					play.innerText = "Pause";
+				}
+			});
+		});
+	}
+	constructor(App: App, fsd: FrameSongDisplay, fwd: FrameWaveDisplay) {
+		const bar = Util.newDiv("bar");
+		const play = Util.newDiv("play");
+		const ff = Util.newDiv("ff");
+		const rw = Util.newDiv("rw");
+		bar.appendChild(ff);
+		bar.appendChild(play);
+		bar.appendChild(rw);
+		this.addEventListeners(App, play, ff, rw, fsd, fwd);
 		
 		const commbarhost = App.getCommbarhost();
 		commbarhost.appendChild(bar);
@@ -203,6 +242,7 @@ class Bar {
 class App {
 	app: HTMLDivElement;
 	audio: HTMLAudioElement;
+	watcherID: number;
 	commbarhost: HTMLDivElement;  // hosting div for comments and play/pause bar
 	tracks: Promise<{
 		[track: string]: {
@@ -227,6 +267,43 @@ class App {
 			}
 		});
 	}
+	// Helper for setupWatcher to find the shadow element.
+	getShadow(fwd: FrameWaveDisplay): HTMLDivElement {
+		const shadows = this.app.getElementsByClassName("shadow");
+		let shadow = null;
+		if (shadows.length < 1) {
+			shadow = Util.newDiv("shadow");
+			shadow.style.position = "absolute";
+			shadow.style.backgroundColor = "rgba(255, 160, 0, 0.5)";
+			fwd.waveDiv.appendChild(shadow);
+		} else
+			shadow = shadows[0];
+		return shadow;
+	}
+	setupWatcher(fwd: FrameWaveDisplay): void {
+		const shadow = this.getShadow(fwd);
+		clearInterval(this.watcherID);
+		this.watcherID = setInterval(() => {
+			const currTime = this.audio.currentTime;
+			const duration = this.audio.duration;
+			const frac = currTime / duration;
+			
+			const cvs = this.app.getElementsByClassName("waveformscanvas");
+			if (cvs.length === 1) {
+				const cv = cvs[0];
+				const cvBCR = cv.getBoundingClientRect();
+				const width = frac * cvBCR.width;
+				const nx = cvBCR.left + window.scrollX;
+				const ny = cvBCR.top + window.scrollY;
+				const height = cvBCR.height;
+				
+				shadow.style.left = nx + "px";
+				shadow.style.top = ny + "px";
+				shadow.style.height = height + "px";
+				shadow.style.width = width + "px";
+			}
+		}, 100);
+	}
 	populateAudio(id: string,
 		fsd: FrameSongDisplay, fwd: FrameWaveDisplay): void {
 		if (this.audio)
@@ -244,7 +321,7 @@ class App {
 			else
 				this.audio.src = track.filepath;
 			this.audio.play();
-			//}
+			this.setupWatcher(fwd);
 		});
 		
 	}
@@ -255,13 +332,12 @@ class App {
 		this.app = Util.newDiv("app");
 		document.body.appendChild(this.app);
 		const fsd = new FrameSongDisplay(this.app);
-		const fwd = new FrameWaveDisplay(this.app);
+		const fwd = new FrameWaveDisplay(this);
 		new FrameCommentsDisplay(this);
 		new Bar(this, fsd, fwd);
 		
 		this.getAudio();
 		this.populateAudio("1", fsd, fwd);
-		//this.tracks.then(stuff => {console.log(stuff);});
 	}
 }
 
